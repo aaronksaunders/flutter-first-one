c33b8b370618170888b58d8f0a18244bed19dcfb
aaron saunders <aaronksaunders@gmail.com>
2018-12-28 01:07:36 -0500

    starting to integrate form and login page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:first_one/screens/MyHomePage.dart';
 import 'package:first_one/screens/MySecondPage.dart';
+import 'package:first_one/screens/auth/LoginPage.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
   final routes = <String, WidgetBuilder>{
     '/': (BuildContext context) => new MyHomePage(title: 'Flutter Home Page'),
+    '/login': (BuildContext context) => new LoginPage(),
     '/second': (BuildContext context) => new MySecondPage(),
   };
 
@@ -12,7 +14,7 @@ class Routes {
     runApp(new MaterialApp(
       title: 'Flutter Demo App',
       theme: ThemeData(primarySwatch: Colors.green),
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: routes,
     ));
   }
---
+++ lib/screens/auth/LoginPage.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Login For Text Application',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              child: MyCustomForm(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Create a Form Widget
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Create a corresponding State class. This class will hold the data related to
+// the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that will uniquely identify the Form widget and allow
+  // us to validate the form
+  //
+  // Note: This is a GlobalKey<FormState>, not a GlobalKey<MyCustomFormState>!
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey we created above
+    return Form(
+      key: _formKey,
+      child: Column(
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding:
+                EdgeInsets.only(left: 20.0, right: 20.0),
+            child: TextFormField(
+              decoration:
+                  InputDecoration(labelText: 'Enter your email address'),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding:
+                EdgeInsets.only(left: 20.0, right: 20.0),
+            child: TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Enter your password'),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, we want to show a Snackbar
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Login User'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 6.0),
+            child: RaisedButton(
+              onPressed: () {
+                _formKey.currentState.reset();
+              },
+              child: Text('Reset Form'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
