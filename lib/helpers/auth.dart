72b4b4d0131802ff6c2a9ceb278760bf3cc8a6db
aaron saunders <aaronksaunders@gmail.com>
2018-12-29 14:07:12 -0500

    added delay using future in auth service

--- lib/helpers/auth.dart
+++ lib/helpers/auth.dart
@@ -4,23 +4,30 @@ class Auth {
   String user;
 
   Future<String> signIn(String email, String password) async {
-    if (email == 'aaron@in3dc.com') {
-      this.user = email;
-      return 'aaron@in3dc.com';
-    }
-    return null;
+    return Future.delayed(const Duration(milliseconds: 200), () {
+      if (email == 'aaron@in3dc.com') {
+        this.user = email;
+        return 'aaron@in3dc.com';
+      }
+      return null;
+    });
   }
 
   Future<String> createUser(String email, String password) async {
-    return null;
+    return Future.delayed(const Duration(milliseconds: 200), () {
+      return this.user;
+    });
   }
 
   Future<String> currentUser() async {
-    return this.user;
+    return Future.delayed(const Duration(milliseconds: 200), () {
+      return this.user;
+    });
   }
 
   Future<void> signOut() async {
-    this.user = null;
-    return;
+    return Future.delayed(const Duration(milliseconds: 200), () {
+      return;
+    });
   }
 }
